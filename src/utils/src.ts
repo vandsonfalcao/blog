6e84a662e17160124a9e787d32b05638c811ba49
Vandson Falcao <vandsonsf@gmail.com>
2025-02-07 10:08:03 -0300

    tunning

--- src/utils/src.ts
+++ src/utils/src.ts
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === "production";
 
 export const src = (url: string) => {
-	if (isProd) return `${process.env.NEXT_PUBLIC_BASE_URL}${url}`;
+	if (isProd) return `${process.env.NEXT_PUBLIC_BASE_PATH}${url}`;
 	return url;
 };
