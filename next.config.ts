a06892fbc2fd8d581527dad01832c2e23d94f74c
Vandson Falcao <vandsonsf@gmail.com>
2025-02-07 04:25:37 -0300

    build: fixing images

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
-// const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-	// basePath: isProd ? "/blog" : "",
+	basePath: isProd ? "/blog" : "",
 	output: "export",
 };
 
