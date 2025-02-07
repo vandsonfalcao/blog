b4867ef786ed82114fc6cfbbcb51a1c8de4c3e5f
Vandson Falcao <vandsonsf@gmail.com>
2025-02-07 04:22:07 -0300

    build: fixing again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,13 @@ export default function Home() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					<Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
+					<Image
+						aria-hidden
+						src="/file.svg"
+						alt="File icon"
+						width={16}
+						height={16}
+					/>
 					Learn
 				</a>
 				<a
@@ -65,7 +71,13 @@ export default function Home() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					<Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
+					<Image
+						aria-hidden
+						src="/window.svg"
+						alt="Window icon"
+						width={16}
+						height={16}
+					/>
 					Examples
 				</a>
 				<a
@@ -74,7 +86,13 @@ export default function Home() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					<Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
+					<Image
+						aria-hidden
+						src="/globe.svg"
+						alt="Globe icon"
+						width={16}
+						height={16}
+					/>
 					Go to nextjs.org →
 				</a>
 			</footer>
